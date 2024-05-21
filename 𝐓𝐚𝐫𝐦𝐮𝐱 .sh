564bd755d0e8bd724962bbf422ae967d9124b2e5
KING BOY <168644865+kingisbak@users.noreply.github.com>
2024-05-21 07:24:47 +0600

    Add files via upload

---
+++ 𝐓𝐚𝐫𝐦𝐮𝐱 .sh
@@ -0,0 +1,406 @@
+banner () {
+clear
+
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo"" 
+echo""
+echo""
+echo""
+echo""
+echo -e '\e[1;32m 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+\e[0m'
+echo""
+echo""
+echo""
+echo""
+echo -e"
+\e[1;32m         /█1%...................../ \e[0m"
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e '\e[1;32m 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "       "/█4%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "        "/█8%......................./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "         "/██10%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "         "/██12%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "         "/███15%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "        "/█████19%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "        "/██████28%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "       "/████████30%................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "        "/██████████40%................/
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "         "/█████████████50%............./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "        "/████████████████60%........../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "       "/███████████████████88%......./
+sleep 0.0
+
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  -e "       "/██████████████████████████████100%....../
+sleep 0.2
+clear
+
+
+echo -e "\033[32m\033[1m
+       
+               ╔═╗┬┌─┐┌┐┌  ╦ ╦┌─┐  
+              \033[33m ╚═╗││ ┬│││  ║ ║├─┘ \033[32m 
+               ╚═╝┴└─┘┘└┘  ╚═╝┴    
+  \033[33m________________________________________________
+  \033[31mWarning:\033[37mDon't Forget Your \033[35mUsernames \033[37mand \033[35mPassword
+  \033[33m________________________________________________
+
+     \033[1m\033[36mTool Name       \033[32m: \033[33mTermux-Login
+     \033[1m\033[36mAuthor_Name     \033[32m: \033[33mNAHID JR 
+     \033[1m\033[36mFacebook \033[32m: \033[33m Md Nahid Hasan 
+     \033[1m\033[36mCountry         \033[32m: \033[33m Bangladesh
+     \033[1m\033[36mState           \033[32m: \033[33m HACKING BOY JR 
+  \033[31m________________________________________________
+"
+
+
+}
+banner 
+
+#!/usr/bin/bash 
+
+bash banner.sh
+echo
+
+read -p $'\e[1;32m  Enter \033[33mUsername \033[37mfor \033[32mSignUp :\e[0m ' username                
+read -p $'\e[1;32m  Enter \033[33mPassword \033[37mfor \033[32mSignUp :\e[0m ' password 
+echo
+echo
+read -p $'\033[1m\033[32m   Your \033[0mNick \033[38;5;209mName \033[31m  : \033[33m\033[1m ' names
+cd                                                   
+cd ..                                               
+cd usr/etc                                       
+rm motd                                           
+rm bash.bashrc                                       
+cat <<LOGIN>bash.bashrc                            
+
+trap '' 2                                          
+echo -e "\e[1;32m
+            ──▄▀▀▀▄───────────────
+           \033[33mPlease Login To Continue\033[32m
+            ──█───█───────────────
+            ─███████─────────▄▀▀▄─
+            ░██─▀─██░░█▀█▀▀▀▀█░░█░
+            ░███▄███░░▀░▀░░░░░▀▀░░
+
+\033[31m           ────────────────────────────
+\033[33m           Login With Your \033[32mCredentials 
+\033[31m           ────────────────────────────
+
+\e[0m"
+echo
+read -p $'       \e[33m\033[1m\033[33m[\033[31m+\033[33m] \033[37mINPUT \033[33mUSERNAME :\033[32m ' user
+read -s -p $'       \e[32m\033[1m\033[33m[\033[31m+\033[33m] \033[37mINPUT \033[33mPASSWORD :\033[33m ' pass                                                
+if [[ \$pass == $password && \$user == $username ]]; then
